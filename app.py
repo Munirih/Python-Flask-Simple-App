78c192706f185bf8c864eb5b4fb17637c4cd1150
Munirih Nganyade <linkmunirih@gmail.com>
2021-11-12 10:36:05 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/stocks/')
+def stocks():
+    return '<h2>Stock List...</h2>'
+
+
+@app.route('/hello/<message>')
+def hello_message(message):
+    return f'<h1>Welcome {escape(message)}!</h1>'
